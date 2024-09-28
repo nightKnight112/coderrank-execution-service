ebebc7a932cb527d0eeed261cfb7c062170d7673
anuragunnikannan <anuragunnikannan7@gmail.com>
2024-09-28 16:15:52 +0530

    added code for reading input from file

--- app.py
+++ app.py
@@ -5,13 +5,28 @@ app = Flask(__name__)
 
 @app.post("/execute")
 def execute():
-    data = request.json
-    filename = data["filename"]
-    input_filename = data["input_filename"]
-    output = subprocess.run(["java", filename], input=input_filename)
-    print(output.stdout)
-    print(output.stderr)
-    return jsonify(1)
+    try:
+        data = request.json
+        filename = data["filename"]
+        input_filename = data["input_filename"]
+        file = ""
+
+        print(filename, input_filename)
+        with open(input_filename, "rb") as f:
+            file = f.read()
+
+        output = subprocess.run(["java", filename], input=file, capture_output=True)
+        stdout = output.stdout.decode().strip()
+        stderr = output.stderr.decode().strip()
+
+        if len(stderr) > len(stdout):
+            return jsonify({"output": stderr})
+        else:
+            return jsonify({"output": stdout})
+        
+    except Exception as e:
+        print(e)
+        return jsonify({"output": "something went wrong"}), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
